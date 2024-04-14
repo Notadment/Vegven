ee0993d8217051de43bdc8c6757c9c65a9caf7f8
Notadment <147793961+Notadment@users.noreply.github.com>
2024-04-14 17:03:49 +0800

    Add a new maven project about spring boot and connect my sql database

---
+++ src/main/java/com/running/vegven/VegvenApplication.java
@@ -0,0 +1,13 @@
+package com.running.vegven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegvenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegvenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/running/vegven/VegvenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.running.vegven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegvenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
